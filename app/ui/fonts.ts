0bc0a1a7845260c76991e1613b4ba1ea789437ae
ryan77710 <lolliaryan@ryan-ordi.local>
2023-10-31 11:43:18 +0100

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana, Bebas_Neue } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const bebasNeue = Bebas_Neue({
+  weight: ["400"],
+  subsets: ["latin"],
+});
